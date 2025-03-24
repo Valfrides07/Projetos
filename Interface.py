1e2194a5fad8d8568022a84a593a5a34b57d789a
Valfrides07 <freitascoc687@gmail.com>
2025-03-24 19:54:36 -0300

    Atualização de interface e integração com o SQLite

--- Interface.py
+++ Interface.py
@@ -1,41 +1,87 @@
 #Projeto de interface
 
 import customtkinter as ctk
+import sqlite3
+from pathlib import Path
+DB_FILE = Path(__file__).parent / 'db.sqlite3'
 
+
+DB_NAME = "db.sqlite3"
+DB_FILE = DB_NAME
+TABLE_NAME = "customers"
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+cursor.execute(
+    f"CREATE TABLE IF NOT EXISTS {TABLE_NAME}"
+    "("
+    "id INTEGER PRIMARY KEY AUTOINCREMENT," "nome TEXT," "senha TEXT"")"
+)
+connection.commit()
+cursor.close()
+connection.close()
+
+
+#Funcionalidade app/ integração com o banco de Dados
+
+def validar_login():
+    usuario = Campo_usuario.get().strip()
+    senha = Campo_senha.get().strip()
+    
+    if not usuario or not senha:
+        mensagem_login.configure(text="Usuário e senha não podem estar vazios!", text_color=" white") 
+    
+    #Conectar uusario no banco de dados
+    
+    connection = sqlite3.connect(DB_FILE)
+    cursor = connection.cursor()
+    
+    # Buscar usuario no banco de Dados
+    cursor.execute(f"SELECT * FROM {TABLE_NAME} WHERE nome = ? AND senha = ?", (usuario,senha))
+    usuario_encontrado = cursor.fetchone()
+    
+    if usuario_encontrado:
+        # Se existir, irá verifcar a senha
+        if usuario_encontrado[2] == senha:
+            mensagem_login.configure(text="ogin realizado com sucesso!", text_color="white")
+        
+        else:
+            mensagem_login.configure(text="Senha incorreta!", text_color="white")
+    else:
+        # Se não existir, cadastrar usuário automaticamente
+        cursor.execute(f"INSERT INTO {TABLE_NAME} (nome , senha) VALUES(?,?)",(usuario, senha))
+        connection.commit()
+        mensagem_login.configure(text="Usuario cadastrado com sucesso!", text_color="white")
+        
+    
+    # Fechar conexão e o cursor
+    cursor.close()
+    connection.close()
+    
+    # Verificação se o usuario foram encontrados
+    # if usuario_encontrado:
+        # mensagem_login.configure (text = 'Login realizado com sucesso!',text_color = 'White')
+        # 
+    # else:
+        # mensagem_login.configure(text='Login ou senha incorretos.',text_color= 'White')
 #Configuração da aparencia
 
 ctk.set_appearance_mode('dark')
 
-#Funcionalidade app
-
-def validar_login():
-    usuario = Campo_usuario.get()
-    senha = Campo_senha.get() 
-    
-    # Verificação se o usuario e senha estão corretos
-    if usuario == 'Valfrides' and senha == '12345':
-        mensagem_login.configure (text = 'Login realizado com sucesso!',text_color = 'White')
-        
-    else:
-        mensagem_login.configure(text='Login incorreto.',text_color= 'White')
-
 #Criação da janela principal
-
 janela_principal = ctk.CTk()
 janela_principal.title('Sistema de Login')
 janela_principal.geometry('300x300')
 
-# Campos 1,2,3
 
 # Label = Informativo acima da barra onde o usuario irá interagir
-Label_usuario = ctk.CTkLabel(janela_principal,text='Usuário')
+Label_usuario = ctk.CTkLabel(janela_principal,text='Login')
 
 # Pady(Distancia) de Usuario até o topo
 Label_usuario.pack(pady = 20)
  
 # Entry = barra de interação onde o usuario irá digitar login
-
-Campo_usuario = ctk.CTkEntry(janela_principal,placeholder_text='Digite o seu usuário')
+Campo_usuario = ctk.CTkEntry(janela_principal,placeholder_text='Seu usuário')
 Campo_usuario.pack(pady = 5)
 
 # Label senha
@@ -43,12 +89,13 @@ Label_senha = ctk.CTkLabel(janela_principal,text='Senha')
 Label_senha.pack(pady = 5)
 
 # Campo Senha
-Campo_senha = ctk.CTkEntry(janela_principal,placeholder_text='Digite a sua senha',show='*')
+Campo_senha = ctk.CTkEntry(janela_principal,placeholder_text='Sua senha',show='*')
 Campo_senha.pack(pady=10)
 
 #Button = Botao de login
 Botao_usuario = ctk.CTkButton(janela_principal,text='Login', command=validar_login)
 Botao_usuario.pack(pady=10)
+
 # Mensagem erro login
 mensagem_login = ctk.CTkLabel(janela_principal, text=' ')
 mensagem_login.pack(pady = 10)
